3014c57b94fe80e5411be8383a3735caa809363c
vladimir <vladimir@blockscout.com>
2023-12-28 01:04:45 +0300

    remove legacy migrations

--- 1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-var Migrations = artifacts.require("Migrations");
-
-module.exports = async function(deployer) {
-  await deployer.deploy(Migrations);
-  const result = await Migrations.deployed();
-  console.log('Migrations: ', result.address);
-};
